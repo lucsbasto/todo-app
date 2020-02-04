c716c4f67ce541991d5b4cec9c938b54fbd1c9d4
Lucas Bastos <lucsbasto@gmail.com>
2020-02-04 16:23:24 -0300

    separando a row em uma função chamada _button

--- lib/main.dart
+++ lib/main.dart
@@ -34,39 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
             "Monday",
             style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
           ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: MaterialButton(
-                  onPressed: () {},
-                  child: Text('Tasks'),
-                  color: Theme.of(context).accentColor,
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(color: Theme.of(context).accentColor),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  padding: EdgeInsets.all(14.0),
-                  textColor: Colors.white,
-                ),
-              ),
-              SizedBox(
-                width: 32,
-              ),
-              Expanded(
-                child: MaterialButton(
-                  onPressed: () {},
-                  child: Text('Events'),
-                  padding: EdgeInsets.all(14.0),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(color: Theme.of(context).accentColor),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  textColor: Theme.of(context).accentColor,
-                  color: Colors.white,
-                ),
-              )
-            ],
-          )
+          _button(context)
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -91,6 +59,42 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _button(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: MaterialButton(
+            onPressed: () {},
+            child: Text('Tasks'),
+            color: Theme.of(context).accentColor,
+            shape: RoundedRectangleBorder(
+              side: BorderSide(color: Theme.of(context).accentColor),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            padding: EdgeInsets.all(14.0),
+            textColor: Colors.white,
+          ),
+        ),
+        SizedBox(
+          width: 32,
+        ),
+        Expanded(
+          child: MaterialButton(
+            onPressed: () {},
+            child: Text('Events'),
+            padding: EdgeInsets.all(14.0),
+            shape: RoundedRectangleBorder(
+              side: BorderSide(color: Theme.of(context).accentColor),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            textColor: Theme.of(context).accentColor,
+            color: Colors.white,
+          ),
+        )
+      ],
     );
   }
 }
