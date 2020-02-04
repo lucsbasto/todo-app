1630e4a5e6f7cf3d59fb7f662de781154c3c9ee7
Lucas Bastos <lucsbasto@gmail.com>
2020-02-04 18:03:42 -0300

    apagando arquivos de fonts não utilizadas

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(primarySwatch: Colors.red, fontFamily: "Montserrat"),
+      home: MyHomePage(),
     );
   }
 }
