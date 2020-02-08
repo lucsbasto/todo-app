a249387ace63691a06d70655df3f4db276fa1e3d
Lucas Bastos <lucasbastos@urpay.com.br>
2020-02-08 17:18:49 -0300

    add complete task and delete task

--- lib/pages/add_task_page.dart
+++ lib/pages/add_task_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:todo_app/widgets/custom_button.dart';
 import 'package:todo_app/widgets/custom_date_time_picker.dart';
 import 'package:todo_app/widgets/custom_modal_action_button.dart';
@@ -10,8 +11,7 @@ class AddTaskPage extends StatefulWidget {
 }
 
 class _AddTaskPageState extends State<AddTaskPage> {
-  String _selectedDate = 'pick date';
-  String _selectedTime = 'pick time';
+  DateTime _selectedDate = DateTime.now();
   Future _pickDate() async {
     DateTime datepick = await showDatePicker(
       context: context,
@@ -19,18 +19,7 @@ class _AddTaskPageState extends State<AddTaskPage> {
       firstDate: new DateTime.now().add(Duration(days: -365)),
       lastDate: new DateTime.now().add(Duration(days: 365)),
     );
-    if (datepick != null) setState(() => _selectedDate = datepick.toString());
-  }
-
-  Future _pickTime() async {
-    TimeOfDay timepick = await showTimePicker(
-      context: context,
-      initialTime: new TimeOfDay.now(),
-    );
-    if (timepick != null)
-      setState(() {
-        _selectedTime = timepick.toString();
-      });
+    if (datepick != null) setState(() => _selectedDate = datepick);
   }
 
   @override
@@ -56,11 +45,7 @@ class _AddTaskPageState extends State<AddTaskPage> {
           CustomDateTimePicker(
               icon: Icons.date_range,
               onPressed: _pickDate,
-              value: _selectedDate),
-          CustomDateTimePicker(
-              icon: Icons.access_time,
-              onPressed: _pickTime,
-              value: _selectedTime),
+              value: DateFormat('dd-MM-yyyy').format(_selectedDate)),
           SizedBox(
             height: 24,
           ),
--- lib/pages/task_page.dart
+++ lib/pages/task_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_app/widgets/custom_button.dart';
 
 class TaskPage extends StatefulWidget {
   @override
@@ -28,48 +29,153 @@ class _TaskPageState extends State<TaskPage> {
       itemCount: _taskList.length,
       itemBuilder: (context, index) {
         return _taskList[index].isDone
-            ? _tasksComplete(_taskList[index].task)
-            : _tasksUncomplete(_taskList[index].task);
+            ? _tasksComplete(_taskList[index])
+            : _tasksUncomplete(_taskList[index]);
       },
     );
   }
 
-  Widget _tasksUncomplete(String task) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20.0, bottom: 24.0),
-      child: Row(
-        children: <Widget>[
-          Icon(
-            Icons.radio_button_unchecked,
-            color: Theme.of(context).accentColor,
-            size: 30,
-          ),
-          SizedBox(
-            width: 24,
-          ),
-          Text(task),
-        ],
-      ),
-    );
-  }
-
-  Widget _tasksComplete(String task) {
-    return Container(
-      foregroundDecoration: BoxDecoration(color: Color(0x60fdfdfd)),
+  Widget _tasksUncomplete(Task task) {
+    return InkWell(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return Dialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child:
+                    Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                  Text(
+                    "Complete Task",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Text(
+                    task.task,
+                    // style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Text(
+                    "Time",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  CustomButtom(
+                    onPressed: () {
+                      //database request
+                    },
+                    buttonText: "Complete",
+                    color: Theme.of(context).accentColor,
+                    textColor: Colors.white,
+                  ),
+                ]),
+              ),
+            );
+          },
+        );
+      },
+      onLongPress: () {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return Dialog(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child:
+                      Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                    Text(
+                      "Delete Task",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    Text(
+                      task.task,
+                      // style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    Text(
+                      "Time",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    CustomButtom(
+                      onPressed: () {
+                        //database request
+                      },
+                      buttonText: "Delete",
+                      color: Theme.of(context).accentColor,
+                      textColor: Colors.white,
+                    ),
+                  ]),
+                ),
+              );
+            });
+      },
       child: Padding(
-        padding: const EdgeInsets.only(left: 20.0, top: 24.0),
+        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
         child: Row(
           children: <Widget>[
             Icon(
-              Icons.radio_button_checked,
+              Icons.radio_button_unchecked,
               color: Theme.of(context).accentColor,
               size: 30,
             ),
             SizedBox(
               width: 24,
             ),
-            Text(task),
+            Text(task.task),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _tasksComplete(Task task) {
+    return InkWell(
+      onTap: () {
+        //complete task
+      },
+      onLongPress: () {
+        //delete task
+      },
+      child: Container(
+        foregroundDecoration: BoxDecoration(color: Color(0x60fdfdfd)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+          child: Row(
+            children: <Widget>[
+              Icon(
+                Icons.radio_button_checked,
+                color: Theme.of(context).accentColor,
+                size: 30,
+              ),
+              SizedBox(
+                width: 24,
+              ),
+              Text(task.task),
+            ],
+          ),
         ),
       ),
     );
